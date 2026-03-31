7fbfba8f81274f74254783e9c9b845c9002918f4
Sara <floreansara24@gmail.com>
2026-03-31 22:15:27 +0300

    initial commit - spring boot setup

---
+++ src/main/java/com/wtbuddy/wtbuddy/WtbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.wtbuddy.wtbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wtbuddy/wtbuddy/WtbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtbuddy.wtbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
