a220c1a2dc61d24490fdcf6626417106c96eae96
Sara <floreansara24@gmail.com>
2026-04-07 23:08:17 +0300

    add global exception handling

---
+++ backend/src/main/java/com/wtbuddy/wtbuddy/exception/AlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.wtbuddy.wtbuddy.exception;
+
+public class AlreadyExistsException extends RuntimeException {
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/com/wtbuddy/wtbuddy/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.wtbuddy.wtbuddy.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
